c9f2ec9a086bd6383542c0df226ec54cbe88e217
di.qin <di.qin@thoughtworks.com>
2020-10-13 15:46:20 +0800

    [QD]: finish add student button.

--- src/App/components/Students.jsx
+++ src/App/components/Students.jsx
@@ -6,17 +6,60 @@ import '../../style/students.scss';
 class Students extends React.Component {
   state = {
     students: [],
+    name: '',
+    inputDisplay: false,
   };
 
   componentDidMount() {
+    this.getAllStudents();
+  }
+
+  getAllStudents = () => {
     axios.get('http://localhost:8080/students').then((res) => {
       this.setState({
         students: res.data,
       });
     });
-  }
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  displayInput = () => {
+    this.setState({ inputDisplay: true });
+  };
+
+  addNewStudent = (keyCode) => {
+    if (keyCode === 13) {
+      axios
+        .post(`http://localhost:8080/student/${this.state.name}`)
+        .then(() => this.getAllStudents());
+      this.setState({
+        name: '',
+        inputDisplay: false,
+      });
+    }
+  };
 
   render() {
+    const addStudent = () => {
+      if (this.state.inputDisplay) {
+        return (
+          <input
+            className="add-student-input"
+            value={this.state.name}
+            onChange={(e) => this.handleInputChange(e)}
+            onKeyUp={() => this.addNewStudent(event.keyCode)}
+          />
+        );
+      }
+      return (
+        <p className="add-student" onClick={() => this.displayInput()}>
+          +添加学员
+        </p>
+      );
+    };
     return (
       <section className="student-list">
         <h2>学员列表</h2>
@@ -24,6 +67,7 @@ class Students extends React.Component {
           {this.state.students.map((cur, index) => {
             return <p className="student" key={index}>{`${cur.id}. ${cur.name}`}</p>;
           })}
+          {addStudent()}
         </main>
       </section>
     );
